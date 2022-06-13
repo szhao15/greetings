996713686166743ce9712a72ef4d7767a692905a
Zhao <zhaos21@zhaos21-c02cg2bvmd6p.ent.core.medtronic.com>
2022-06-13 12:08:53 -0500

    %s

--- greetings.go
+++ greetings.go
@@ -3,7 +3,7 @@ package greetings
 import "fmt"
 
 func Hello(name string) string {
-	message := fmt.Sprintf("Hi, %v. welcome!", name)
+	message := fmt.Sprintf("Hi, %s. welcome!", name)
 	fmt.Println("Printing from the function:")
 	fmt.Println(message)
 	return message
