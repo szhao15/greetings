d983f70a98efd2baacd48db76a1a1672c48c9b73
Zhao <zhaos21@zhaos21-c02cg2bvmd6p.ent.core.medtronic.com>
2022-06-13 11:57:17 -0500

    Debugging

--- greetings.go
+++ greetings.go
@@ -4,5 +4,7 @@ import "fmt"
 
 func Hello(name string) string {
 	message := fmt.Sprintf("Hi, %v. welcome!", name)
+	fmt.Println("Printing from the function:")
+	fmt.Println(message)
 	return message
 }
